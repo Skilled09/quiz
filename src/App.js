bb9ea8afd9e01d101fb4eb09045222a6ec906793
Skilled09 <146964957+Skilled09@users.noreply.github.com>
2023-10-22 17:06:01 +0500

    created quiz js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './App.css'
+import Quiz from './pages/Quiz'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Quiz/>
+    </div>
   )
 }
